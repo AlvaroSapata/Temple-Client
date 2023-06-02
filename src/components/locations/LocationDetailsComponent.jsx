71384a7124f34e42febc934da39dd6791e478c8d
Alvaro Martinez <alvaromfdv@gmail.com>
2023-06-02 17:50:33 +0200

    added map

--- src/components/locations/LocationDetailsComponent.jsx
+++ src/components/locations/LocationDetailsComponent.jsx
@@ -4,34 +4,73 @@ import {
   deleteLocationService,
   getAllLocationsService,
   editLocationService,
-  getLocationDetailsService
+  getLocationDetailsService,
 } from "../../services/locations.services";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 
 function LocationDetails() {
+  
   const params = useParams();
+  console.log(params)
   const navigate = useNavigate();
 
   const [locationDetails, setLocationDetails] = useState(null);
   // Estado de loading
   const [isLoading, setIsLoading] = useState(true);
+  // Leaflet mapa
+  const [ center, setCenter ] = useState([42.34, -3.71])
 
-  useEffect(()=>{
-    getData()
-  },[])
+  const [clickedPosition, setClickedPosition] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const getData = async () => {
     try {
-    const response = await getLocationDetailsService(params.id);
+      const response = await getLocationDetailsService(params.locationId);
 
-    console.log(response);
-
-    }
-    catch (error) {
+      console.log(response);
+      setLocationDetails(response.data);
+      setIsLoading(false);
+    } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await deleteLocationService(params.id);
+      navigate("/locations");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  if (isLoading) {
+    return <h3>...buscando</h3>;
   }
 
-  return <div>LocationDetails COmponent</div>;
+  return <div>
+
+    <h3>Detalles de la Ubicacion</h3>
+    <div>
+      <p>{locationDetails.name}</p>
+      <p>{locationDetails.description}</p>
+      <img src={locationDetails.image} alt="imagen" width={"200px"} />
+    </div>
+    <MapContainer center={center} zoom={18} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+
+  {/* invoke Marker Componentes here */}
+
+</MapContainer>
+    <button >Borrar</button>
+
+  </div>;
 }
 
 export default LocationDetails;
